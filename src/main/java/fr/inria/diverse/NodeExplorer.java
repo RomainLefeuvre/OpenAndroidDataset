adb8486138c568a554e0ad336780e57b19c9f754
Lefeuvre Romain <romain.lefeuvre@univ-rennes1.fr>
2022-10-10 15:07:37 +0200

    fixing license

--- src/main/java/fr/inria/diverse/NodeExplorer.java
+++ src/main/java/fr/inria/diverse/NodeExplorer.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2020-2022 The Software Heritage developers
- * See the AUTHORS file at the top-level directory of this distribution
- * License: GNU General Public License version 3, or any later version
- * See top-level LICENSE file for more information
- */
-
 package fr.inria.diverse;
 
 import com.google.gson.Gson;
