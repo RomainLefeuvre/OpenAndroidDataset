148649c1800b1e27ffaa32dbfa5e916f4c4e30ce
Lefeuvre Romain <romain.lefeuvre@univ-rennes1.fr>
2022-11-04 16:46:26 +0100

    updating workflow

--- scripts/install_swh_graph_to_local_m2.sh
+++ scripts/install_swh_graph_to_local_m2.sh
@@ -2,6 +2,6 @@
 mvn install:install-file -Dfile=dsiutils/dsiutils-2.7.3-SNAPSHOT.jar -DpomFile=dsiutils/pom.xml
 git clone https://github.com/RomainLefeuvre/swh-graph
 cd swh-graph/java/
-mvn clean install --no-transfer-progress
+mvn --no-transfer-progress clean install
 cd ../../
 rm -r -f ./swh-graph/
