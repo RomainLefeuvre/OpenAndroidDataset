470668cb531c490da5b90d25473217111a64fbcc
Lefeuvre Romain <romain.lefeuvre@univ-rennes1.fr>
2022-11-09 17:57:04 +0100

    Debug ...

--- src/main/java/fr/inria/diverse/Main.java
+++ src/main/java/fr/inria/diverse/Main.java
@@ -1,24 +1,48 @@
 package fr.inria.diverse;
 
 import fr.inria.diverse.tools.Configuration;
+import it.unimi.dsi.bits.Fast;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+
+import static it.unimi.dsi.big.webgraph.labelling.BitStreamArcLabelledImmutableGraph.LABELS_EXTENSION;
 
 public class Main {
     static Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException, InterruptedException {
+
+
         if (args.length > 0) {
             Configuration.init(args[0]);
         } else {
             Configuration.init();
         }
         Graph g = new Graph();
+        long currentNodeId = 23000000000L;
+        long size = 0L;
         g.loadGraph();
+        FileInputStream fis = new FileInputStream(Configuration.getInstance()
+                .getGraphPath() + "-labelled" + LABELS_EXTENSION);
+        try {
+            size = fis.getChannel().size();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long l = Fast.mostSignificantBit((size * Byte.SIZE + 1) / (g.getGraph().numNodes() + 1));
+        long a = l * currentNodeId;
+        logger.info("For node " + currentNodeId + " a is " + a + " l is " + l);
+        if (a >>> 6 > Integer.MAX_VALUE) {
+
+            logger.error("Will crash node " + currentNodeId);
+        }
+        logger.info(g.getGraph().numNodes());
+        //ArcLabelledNodeIterator.LabelledArcIterator it = g.getGraph().labelledSuccessors(25000000000L);
         /*GraphExplorer debugExplorer = new DebugExplorer(g);
         debugExplorer.run();
         */
